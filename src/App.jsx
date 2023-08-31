39f9a74ac0ae2aeecdc2711a1139f3a736d6138b
Kacper <kacpercybulski@icloud.com>
2023-08-31 16:34:48 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -30,11 +30,32 @@ function App() {
 					</p>
 				</section>
 
-				<h2 className="interests">Interesets</h2>
-				<p className="facts">
-					Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture
-					ninja. Coffee fanatic.
-				</p>
+				<section className="second">
+					<h2 className="interests">Interests</h2>
+					<p className="info">
+						Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture
+						ninja. Coffee fanatic.
+					</p>
+				</section>
+
+				<footer className="footer">
+					<div className="footer-icons">
+						<a href="#" className="twitter">
+							<img src="./images/Twitter Icon.png" alt="Twitter icon" />
+						</a>
+
+						<a href="#" className="facebook">
+							<img src="images/Facebook Icon.png" alt="Facebook Icon" />
+						</a>
+
+						<a href="#" className="instagram">
+							<img src="./images/Instagram Icon.png" alt="Instagram Icon" />
+						</a>
+						<a href="#" className="github">
+							<img src="./images/GitHub Icon.png" alt="Github Icon" />
+						</a>
+					</div>
+				</footer>
 			</div>
 		</>
 	)
