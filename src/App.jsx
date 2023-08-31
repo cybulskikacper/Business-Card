4e186f89abffc3bd33ea2b0b5c43b376982fd272
Kacper <kacpercybulski@icloud.com>
2023-08-31 14:35:02 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,38 @@ import './App.css'
 
 function App() {
 	return (
-		<div className="card">
-			<img src="./images/Rectangle 90.jpg" alt="Picture of young woman smilling" className="picture"></img>
+		<>
+			<div className="card">
+				<img src="./images/Rectangle 90.jpg" alt="Picture of young woman smilling" className="picture"></img>
 
-			<h3 className="name">Laura Smith</h3>
-			<h5 className="profession">Frontend developer</h5>
-			<h6 className="website">laurasmith.website</h6>
+				<h3 className="name">Laura Smith</h3>
+				<h5 className="profession">Frontend developer</h5>
+				<h6 className="website">laurasmith.website</h6>
 
-			<button className="email"><img src="./images/Mail.png" alt="Mail icon"/>Email</button>
-			<button className="linkedin">Linkedin</button>
-		</div>
+				<div className="buttons">
+					<button className="email" aria-hidden="true">
+						Email
+					</button>
+					<button className="linkedin" aria-hidden="true">
+						Linkedin
+					</button>
+				</div>
+
+				<section className="first">
+					<h2 className="about">About</h2>
+					<p className="me">
+						I am a frontend developer with a particular interest in making things simple and automating daily tasks. I
+						try to keep up with security and best practices, and am always looking for new things to learn.
+					</p>
+				</section>
+
+				<h2 className="interests">Interesets</h2>
+				<p className="facts">
+					Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture
+					ninja. Coffee fanatic.
+				</p>
+			</div>
+		</>
 	)
 }
 
